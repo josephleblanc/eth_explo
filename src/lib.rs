388df7383944ffc5f351b90c6dc450ddbb725136
Joseph LeBlanc <joseph.t.leblanc@protonmail.com>
2022-04-03 02:04:21 -0700

    initial commit, more or less

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,268 @@
-pub fn hex_to_u32(h: &str) -> u32 {
-    println!("h: {}", h);
-    h.chars()
-        .rev()
-        .enumerate()
-        .fold(0, |acc, (i, x)| acc + 
-              ((16_u32.pow(i as u32) ) * (x.to_digit(16).unwrap() as u32)))
+use web3::types::{
+    Transaction,
+    TransactionReceipt,
+    H256,
+    U256,
+    H160,
+    Log,
+};
+
+#[allow(dead_code)]
+#[allow(unused)]
+
+pub fn scrape_logs(logs: &Vec<Log>, fid_vec: &Vec<H256>, final_recipient: H256,
+                   amount_out_min: U256)
+// Required outputs:
+// amount traded out, 
+// pool ratios
+-> (f64, f64, Vec<(f64, f64)>)
+{
+    let mut pool_ratios: Vec<(f64, f64)> = vec![];
+    let mut end_amount = U256::from_big_endian(&[0_u8; 32]);
+    let mut start_amount = U256::from_big_endian(&[0_u8; 32]);
+    let mut approve = false;
+    for log in logs {
+        let function_hash = log.topics[0];
+        if function_hash == fid_vec[5] {
+            approve = true;
+        } if function_hash == fid_vec[2] { // sync_fid
+            let data_vec = get_bytes_vec(&log.data.0)
+                .iter()
+                .map(|entry| u256_to_f64(U256::from_big_endian(entry)))
+                .collect::<Vec<f64>>();
+            pool_ratios.push((data_vec[0], data_vec[1]));
+        } else if function_hash == fid_vec[3] { // swap_fid
+            let data_vec = get_bytes_vec(&log.data.0)
+                .iter()
+                .map(|entry| U256::from_big_endian(entry))
+                .collect::<Vec<U256>>();
+            let to = log.topics[2];
+            if final_recipient == to {
+                let amount_out = std::cmp::max(data_vec[2], data_vec[3]);
+                end_amount = match amount_out > amount_out_min {
+                    true => amount_out,
+                    false => end_amount,
+                };
+            }
+            if start_amount.low_u32() == 0_u32 {
+                start_amount = std::cmp::max(data_vec[0], data_vec[1])
+            }
+        } else if function_hash == fid_vec[4] { // withdrawal_fid
+            let src = log.topics[1];
+            let wad: U256 = get_bytes_vec(&log.data.0)
+                .iter()
+                .map(|entry| U256::from_big_endian(entry))
+                .last()
+                .unwrap();
+            if final_recipient == src || approve == true {
+                end_amount = wad;
+            }
+        }
+    }
+    (u256_to_f64(start_amount), u256_to_f64(end_amount), pool_ratios)
 }
 
-pub fn decode_blockNumber(res: &str) -> u32 {
-    let block_number = res
-        .split(['"', '\\', ',', ':', '{', '}'])
-        .filter(|item| !item.is_empty())
-        .last()
-        .unwrap();
 
-    let block_number = &block_number[2..];
-    println!("block_number: {}", block_number);
-
-    hex_to_u32(block_number)
-}
-
-pub fn get_block_by_number(block_number: &str, hydrate: bool, endpoint: &str, mut output: String)
--> String {
-    let hydrate = match hydrate {
-        true => "true",
-        false => "false",
+// Method ID: 7ff36ab5
+// Function: swapExactETHForTokens(uint256 amountOutMin, 
+//                                  address[] path, 
+//                                  address to, 
+//                                  uint256 deadline)
+// Method ID: fb3bdb41
+// Function: swapETHForExactTokens(uint256 amountOut, 
+//                                  address[] path, 
+//                                  address to, 
+//                                  uint256 deadline)
+pub fn read_uniswap_tx(tx: &Transaction, receipt: &TransactionReceipt,
+                                   fid_vec: &Vec<H256>, 
+                                   short_input_funcs: &[&str])
+-> Option<(Option<H160>,   //start_token
+    f64,           // start_amount
+    Option<H160>, // end_token
+    f64,
+    H160, //receiving_addr
+    Vec<((H160, H160), (f64, f64))>)> // pool_ratios
+{
+    // exit early if no last log
+    if receipt.logs.last().is_none() { 
+        println!("REVERTED: Transaction has no logs, probably reverted.");
+        return None; 
+    } // exit early if no last log
+    let method = hex::encode(&tx.input.0[0..4]);
+    let is_eth_input = short_input_funcs.contains(&method.as_str());
+    let input_offset = match is_eth_input {
+        true => 0_usize,
+        false => 1_usize,
     };
-    let client = reqwest::Client::new();
-    let payload = format!(
-"{{
-    \"jsonrpc\": \"2.0\",
-    \"method\": \"eth_getBlockByNumber\",
-    \"params\": [\"{}\", {}],
-    \"id\": \"0\"
-}}",
-        block_number, hydrate);
-    output = client.post(endpoint)
-        .body(payload)
-        .send()
-        .unwrap()
-        .text()
-        .unwrap();
 
-    output
+    let inputs_u8: Vec<&[u8]> = get_bytes_vec(&tx.input.0[4..]);
+    let amount_out_min: U256 = match method.as_str() {
+        // swapExactETHForTokens
+        "7ff36ab5" 
+        | "b6f9de95" => U256::from_big_endian(inputs_u8[0]), 
+        "791ac947" 
+        | "18cbafe5" 
+        | "38ed1739" => U256::from_big_endian(inputs_u8[1]),
+        _ => U256::from_dec_str(&"0").unwrap()
+    };
+    let end_token: Option<H160> = Some(H160::from_slice(&inputs_u8.last().unwrap()[12..]));
+    let receiving_addr = H160::from_slice(&inputs_u8[2+input_offset][12..]);
+    let start_token = match is_eth_input {
+        true => None,
+        false => Some(H160::from_slice(&inputs_u8[5+input_offset][12..])),
+    };
+   
+    let raw_swap_addrs = &inputs_u8[5+input_offset..];
+    
+    // When ordering pairs, remember uniswap orders them by which by 
+    // (lesser, greater) for (token_0, token_1)
+    let swap_addrs = raw_swap_addrs.iter()
+        .zip(raw_swap_addrs.iter()
+             .skip(1))
+        .map(|(token_0, token_1)| (H160::from_slice(&token_0[12..]),
+                                   H160::from_slice(&token_1[12..])))
+        .map(|(token_0, token_1)| match token_0 < token_1 {
+            true => (token_0, token_1),
+            false => (token_1, token_0),
+            })
+        .collect::<Vec<(H160, H160)>>();
+
+    let final_recipient = H256::from_slice(inputs_u8[2+input_offset]);
+    let (start_amount, end_amount, reserve_ratios) = 
+        scrape_logs(&receipt.logs, fid_vec, final_recipient, amount_out_min);
+    let pool_ratios = swap_addrs.into_iter()
+        .zip(reserve_ratios)
+        .collect::<Vec<((H160, H160), (f64, f64))>>();
+
+    return Some((start_token, start_amount, end_token, end_amount, receiving_addr, pool_ratios)); 
 }
+
+//// Method ID: 791ac947
+//// Function: swapExactTokensForETHSupportingFeeOnTransferTokens(
+////                                                  uint256 amountIn, 
+////                                                  uint256 amountOutMin, 
+////                                                  address[] path, 
+////                                                  address to, 
+////                                                  uint256 deadline)
+//pub fn print_swapExactTokensForETHSupportingFeeOnTransferTokens(tx: &Transaction, 
+//                                      receipt: &TransactionReceipt,
+//                                      fid_vec: &Vec<H256>) {
+//// Outputs desired: 
+//// Amount traded in, 
+//// amount traded out, 
+//// pool ratios, 
+//// receiving address,
+//// start token, 
+//// end token
+//    println!("swapExactTokensForETHSupportingFeeOnTransferTokens");
+//    println!("tx hash: {:?}", tx.hash);
+//    println!("methodId: {}", hex::encode(&tx.input.0[0..4]));
+//    println!("from{:?}\nto: {:?}", tx.from, tx.to);
+//    let inputs_u8 = get_bytes_vec(&tx.input.0[4..]);
+//    println!("starting token: {}\namountIn tokens:{}",
+//             H160::from_slice(&inputs_u8[6][12..]),
+//             U256::from_big_endian(inputs_u8[0]));
+//    println!("end token : {:?}", H160::from_slice(
+//            &inputs_u8.last().unwrap()[12..]));
+//    let last_log = match receipt.logs.last() { 
+//        Some(logs) => logs.data.0.as_slice(),
+//        None => return println!("REVERTED: No receipt log, probably reverted"),
+//    };
+//    let vec_last_log = get_bytes_vec(last_log)
+//        .iter()
+//        .map(|entry| U256::from_big_endian(entry))
+//        .collect::<Vec<U256>>();
+//    let mut final_recipient = H256::from_slice(inputs_u8[2]);
+//    for log in &receipt.logs {
+//        final_recipient = match print_decoded_log(log, fid_vec, &tx.from.unwrap(), final_recipient) {
+//            Some(addr_U256) => addr_U256,
+//            None => final_recipient,
+//        };
+//    }
+//    println!("get_log_final_amount: {:?}", 
+//             get_log_final_amount(&receipt.logs.iter().last().unwrap(), fid_vec));
+//}
+//
+//// Method ID: b6f9de95
+//// Function: swapExactETHForTokensSupportingFeeOnTransferTokens(
+////                                          uint256 amountOutMin, 
+////                                          address[] path, 
+////                                          address to, 
+////                                          uint256 deadline)
+//pub fn print_swapExactETHForTokensSupportingFeeOnTransferTokens(tx: &Transaction,
+//                                        receipt: &TransactionReceipt,
+//                                        fid_vec: &Vec<H256>) {
+//// Outputs desired: 
+//// Amount traded in, 
+//// amount traded out, 
+//// pool ratios, 
+//// receiving address,
+//// start token, 
+//// end token
+//    println!("swapExactETHForTokensSupportingFeeOnTransferTokens");
+//    println!("tx hash: {:?}", tx.hash);
+//    println!("methodId: {}", hex::encode(&tx.input.0[0..4]));
+//    println!("from{:?}\nto: {:?}", tx.from, tx.to);
+//    let inputs_u8 = get_bytes_vec(&tx.input.0[4..]);
+//    println!("Exact ETH in: {}\namountOutMin tokens:{}",
+//             tx.value,
+//             U256::from_big_endian(inputs_u8[0]));
+//    println!("end token : {:?}", H160::from_slice(
+//            &inputs_u8.last().unwrap()[12..]));
+//    let last_log = match receipt.logs.last() { 
+//        Some(logs) => logs.data.0.as_slice(),
+//        None => return println!("REVERTED: No receipt log, probably reverted"),
+//    };
+//    let vec_last_log = get_bytes_vec(last_log)
+//        .iter()
+//        .map(|entry| U256::from_big_endian(entry))
+//        .collect::<Vec<U256>>();
+//    for log in &receipt.logs {
+//        let final_recipient = H256::from_slice(inputs_u8[2]); // different than normal [2]
+//        print_decoded_log(log, fid_vec, &tx.from.unwrap(), final_recipient);
+//    }
+//    println!("get_log_final_amount: {:?}", 
+//             get_log_final_amount(&receipt.logs.iter().last().unwrap(), fid_vec));
+//
+//}
+pub fn get_bytes_vec(inputs: &[u8]) -> Vec<&[u8]> {
+    let index_inputs = inputs.len() / 32;
+    
+    let mut vec_index: Vec<&[u8]> = vec![];
+    for index in (1..=index_inputs).map(|i| i*32) {
+        vec_index.push(&inputs[(index-32)..index]);
+    }
+    vec_index
+}
+
+
+fn poor_man_log_10(mut n: U256) -> usize {
+    let mut places: usize = 0;
+    let ten = U256::from(10_u64);
+    while !n.is_zero() {
+        n = n.checked_div(ten).unwrap();
+            places += 1;
+    }
+    places
+}
+
+fn highest_five(mut n: U256) -> (U256, usize) {
+    let places = poor_man_log_10(n);
+    if places >= 16 {
+        let ten_k = U256::exp10(places - 16);
+        n = n.checked_div(ten_k).unwrap();
+    }
+    (n, places)
+}
+
+pub fn u256_to_f64(n: U256) -> f64 {
+    let (top_five, places) = highest_five(n);
+    match places >= 16 {
+        true => (top_five.as_usize() as f64) * 10_f64.powf(places as f64 - 16.0),
+        false => top_five.as_usize() as f64
+    }
+}
+
+//pub fn coin_to_weth(start_amount: U256, weth_val: U256, coin_val: U256) -> f64 {
+//    start_amount(
+//}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,273 @@
-use std::fs;
-use std::fs::File;
-use std::io::prelude::*;
-use eth_explo::{hex_to_u32, 
-    decode_blockNumber,
-    get_block_by_number
-};
-
-fn main() {
-    // Endpoint file is in .gitignore to avoid sharing sensitive information
-    // on github
-    let endpoint = fs::read_to_string("../config/endpoint.txt")
-        .unwrap();
-    let endpoint: &str = endpoint.trim();
-    println!("{:?}", endpoint);
-
-//    let client = reqwest::Client::new();
-//    let res: String = client.post(endpoint)
-//        .body(
-//r#"{
-//    "jsonrpc": "2.0",
-//    "method": "eth_getBlockByNumber",
-//    "params": ["0xdc0ba9", true],
-//    "id": "0"
-//}"#
-//        )
-//        .send()
-//        .unwrap()
-//        .text()
-//        .unwrap();
+// Important information to extract:
+//      Keeping track of trade:
+//          x Amount traded in
+//          x Amount traded out
+//          Uniswap pool ratio at time of exchange
+//          x Sending address
+//          x Receiving address (if WETH, make sure it counts as a coin they own)
+//          Bool sending addr == receiving addr
+//          x Start token
+//          x End token
 //
-//    let res = format!("{}", res);
+//      Trader profile:
+//          Trade frequency
+//          Total amount in
+//          Total amount out
+//          Percentage profit
+//          
+//      Interesting Traders Profile
+//          Use API to get total trade history
+//
+//      Uniswap pools
+//          Pool addr for each token
+//          New pool ratio after each trade
 
-    let block_number = "0xdc0ba9";
-    let hydrate = true;
-    let res: String = String::new();
-    let res = get_block_by_number(block_number, hydrate, endpoint, res);
+use std::collections::HashMap;
+use std::fmt::Debug;
+use std::thread;
+use std::time::{Instant, Duration};
+use std::env;
 
-    println!("response: {}", res);
+use hex::FromHex;
 
-    let mut file = File::create("hydrated.txt").unwrap();
+use web3::types::{H160, BlockId, U64, H256};
 
-    file.write_all(res.as_bytes()).unwrap();
+use eth_explo::{
+    read_uniswap_tx,
+    u256_to_f64
+};
+//    print_swapETHForExactTokens, // Leaving these here for testing
+//    print_swapExactTokensForETH, //
+//    print_swapExactTokensForTokens, //
+//    print_swapTokensForExactTokens, //
+//    print_swapExactTokensForETHSupportingFeeOnTransferTokens, //
+//    print_swapExactETHForTokensSupportingFeeOnTransferTokens, //
+//    };
+#[allow(non_snake_case)]
+
+#[tokio::main]
+async fn main() -> web3::Result<()> {
+    dotenv::dotenv().ok();
+
+    // Set method ids for input functions, hash of first 8 hex digits in keccak hash
+    let swapExactETHForTokens = "7ff36ab5";
+    let swapETHForExactTokens = "fb3bdb41";
+
+    let eth_for_ids: [&str; 2] = [
+        swapExactETHForTokens,
+        swapETHForExactTokens
+    ];
+
+    let swapExactTokensForTokens = "38ed1739";
+    let swapExactTokensForETH = "18cbafe5";
+    let swapTokensForExactTokens = "8803dbee";
+    let swapExactTokensForETHSupportingFeeOnTransferTokens = "791ac947";
+    let method_ids: [&str; 6] = [
+        swapExactETHForTokens,
+        swapETHForExactTokens,
+        swapExactTokensForTokens,
+        swapExactTokensForETH,
+        swapTokensForExactTokens,
+        swapExactTokensForETHSupportingFeeOnTransferTokens
+    ];
+    // Set function IDs for functions called in logs.
+    let deposit_fid = H256::from_slice(Vec::from_hex(
+            "e1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c").unwrap()
+            .as_slice());
+    let transfer_fid = H256::from_slice(Vec::from_hex(
+            "ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef").unwrap()
+            .as_slice());
+    let sync_fid = H256::from_slice(Vec::from_hex(
+            "1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1").unwrap()
+            .as_slice());
+    let swap_fid = H256::from_slice(Vec::from_hex(
+            "d78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822").unwrap()
+            .as_slice());
+    let withdrawal_fid = H256::from_slice(Vec::from_hex(
+            "7fcf532c15f0a6db0bd6d0e038bea71d30d808c7d98cb3bf7268a95bf5081b65").unwrap()
+            .as_slice());
+    let approval_fid = H256::from_slice(Vec::from_hex(
+            "8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925").unwrap()
+            .as_slice());
+    let fid_vec = vec![deposit_fid, 
+                        transfer_fid, 
+                        sync_fid, 
+                        swap_fid, 
+                        withdrawal_fid,
+                        approval_fid];
+
+    // Eth does not have an address, so just use WETH address instead
+    let weth_addr = H160::from_slice(Vec::from_hex(
+        "c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2").unwrap()
+            .as_slice());
+
+    let debug_address = H160::from_slice(Vec::from_hex(
+        "34ed8d7b93485b454a57f856c54be08ade3c9132").unwrap()
+        .as_slice());
+
+//    // Use this address for value at time of trade, calculated in WETH
+//    let hist_value_addr = H160::from_slice(Vec::from_hex(
+//            "0000000000000000000000000000000000000000").unwrap()
+//            .as_slice());
+
+    // Set up web3 endpoint
+    let uniswap_addr = H160::from_slice(
+        &hex::decode(b"7a250d5630B4cF539739dF2C5dAcb4c659F2488D").unwrap());    
+    let transport = web3::transports::http::Http::new(
+        &env::var("ARCHIVE_NODE").unwrap())
+        .unwrap();
+    let web3 = web3::Web3::new(transport);
+
+    // Set up trader tracking and uniswap tracking
+    let mut trader_kv: HashMap<H160, HashMap<H160, f64>> = HashMap::new();
+    let mut uniswap_pools: HashMap<(H160, H160), (f64, f64)> = HashMap::new();
+    let mut trader_hist_cost: HashMap<H160, f64> = HashMap::new();
+
+    // Time stuff
+    let mut start_t = Instant::now();
+    let one_sec = Duration::from_millis(1_000);
+    let mut call_count = 0;
+
+    // Block range
+    let start_block = 14503000_u64;
+    let end_block = 14503100_u64;
+    let n_blocks = end_block - start_block;
+    for number in start_block..end_block {
+        println!("Scanning block {}     of  {}", number - start_block, n_blocks);
+        let block_number = BlockId::from(U64::from(number));
+        let block_txs = web3.eth().block_with_txs(block_number).await?
+            .unwrap()
+            .transactions;
+        call_count += 1;
+        println!("call_count: {}/10", call_count);
+        for tx in block_txs.iter() {
+            if tx.to.is_some() && tx.to.unwrap() == uniswap_addr { // test for last_log somewhere here
+                let method_id = hex::encode(&tx.input.0[0..4]);
+                if method_ids.contains(&method_id.as_str()) {
+                    // Find transactions with the uniswap router using the commands in method_ids
+                    // update trader hashmap
+                    // update uniswap hashmap
+                    let receipt = web3.eth().transaction_receipt(tx.hash).await?.unwrap();
+                    call_count += 1;
+//                    println!("call_count: {}/10", call_count);
+//                    println!("method_id: {}", method_id.as_str());
+//                    println!("{:?}", tx.hash);
+                    if receipt.logs.last().is_some() {
+                    let (start_token, start_amount, end_token, end_amount, receiving_addr, pool_ratios) = 
+                        read_uniswap_tx(tx, 
+                                        &receipt, 
+                                        &fid_vec, 
+                                        &eth_for_ids).unwrap();
+//                    println!(
+//"start_token:{:?}\nstart_amount:{:?}\nend_token:{:?}
+//end_amount:{:?}\nreceiving_addr:{:?}\npool_ratios:{:?}",
+//start_token, start_amount, end_token, end_amount, receiving_addr, pool_ratios
+//                    );
+//                    println!("send_addr == receiving_addr: {}", tx.from.unwrap() == receiving_addr);
+                    let from_addr = H160::from(tx.from.expect("Every tx should have this field").0);
+                    let start_token = start_token.unwrap_or(weth_addr);
+//                    println!("start_token assigned to weth: {}", weth_addr);
+
+                    let trader_entry = trader_kv.entry(from_addr).or_insert(HashMap::new());
+                    *trader_entry.entry(start_token)
+                        .or_insert(-1.0 * start_amount) 
+                        -= start_amount;
+                    let gas_fee_f64 = u256_to_f64(receipt.gas_used
+                                        .expect("every successful transaction requires gas"));
+                    *trader_entry.entry(weth_addr)
+                        .or_insert(-1.0 * gas_fee_f64)
+                        -= gas_fee_f64;
+                    *trader_entry.entry(end_token.unwrap())
+                        .or_insert(end_amount)
+                        += end_amount;
+
+                    if tx.from == Some(debug_address) {
+                        println!("TRANSACTION\n{:?}", tx.from);
+                    }
+
+                    for (coin_pair, value_pair) in pool_ratios { // try only saving weth tuples
+                        if coin_pair.0 == weth_addr || coin_pair.1 == weth_addr {
+                            uniswap_pools.insert(coin_pair, value_pair);
+//                            println!("Inserting to uniswap_pools: {:?}", (coin_pair, value_pair));
+
+                            let hist_val = match coin_pair.0 == weth_addr {
+                                true => (end_amount * value_pair.0 / value_pair.1) + gas_fee_f64,
+                                false => (end_amount * value_pair.1 / value_pair.0) + gas_fee_f64,
+                            };
+//                            println!("hist_val: {}", hist_val);
+                            *trader_hist_cost.entry(tx.from.expect("All valid transactions have a sender"))
+                                .or_insert(hist_val)
+                                += hist_val;
+                        }
+                    }
+
+                    } else {
+//                        println!("REVERTED: NO LOG DATA, PROBABLY REVERTED");
+                    }
+                }
+//                println!("");
+            }
+            // Rate limiting
+            if call_count >= 9 {
+                println!("call_count = {}/10", call_count);
+                let time_passed = start_t.elapsed();
+                println!("TIME CHECK: time passed = {} millis", time_passed.as_millis());
+                if time_passed < one_sec {
+                    println!("NAP TIME: {:?}", one_sec - time_passed);
+                    thread::sleep(one_sec - time_passed);
+                }
+                start_t = Instant::now();
+                call_count = 0;
+                println!("call_count: {}/10", call_count);
+            }
+        }
+    }
+    for entry in &uniswap_pools {
+        println!("{:?}", entry);
+    }
+    // Calculate the ratio of 
+    let uniswap_ratios: HashMap<&H160, f64> = uniswap_pools.iter()
+        .filter_map(|((coin0, coin1), (res0, res1))| match &weth_addr == coin0 {
+            true => Some((coin1, *res0 / *res1)),
+            false => match coin1 == &weth_addr {
+                true  => Some((coin0, *res1 / *res0)),
+                false => None,}})
+        .collect();
+//    for entry in &uniswap_ratios {
+//        println!("uniswap_ratios");
+//        println!("{:?}", entry);
+//    }
+
+    let mut trader_profit_list: Vec<(&H160, f64)> = trader_kv.iter()
+        .map(|(address, portfolio)| (address, portfolio.iter() 
+             .filter_map(|(coin, amt)| debug_print(coin).then(|| match uniswap_ratios.get(coin) {
+                         Some(ratio) => Some(amt * ratio),
+                         None => None }).unwrap()) 
+             .fold(0_f64, |acc, x| acc + x)))
+        .collect::<Vec<(&H160, f64)>>();
+    trader_profit_list.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+//    for entry in &trader_profit_list {
+//        println!("{:?}", entry);
+//    }
+
+    println!("RAW PROFIT");
+    let mut trader_profit_percent = trader_profit_list
+        .iter()
+        .filter(|(addr, _)| trader_hist_cost.contains_key(addr))
+        .map(|(addr, profit)| (addr, profit / trader_hist_cost[addr]))
+        .filter(|(_, percent)| percent.is_normal())
+        .collect::<Vec<(&&H160, f64)>>();
+    trader_profit_percent.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+
+    println!("\nPROFIT BY PERCENT");
+    for entry in trader_profit_percent {
+        println!("{:?}", entry);
+    }
+    Ok(())
 }
 
+fn debug_print<T: Debug>(to_print: T) -> bool {
+    println!("DEBUG PRINT");
+    println!("\t{:?}", to_print);
+    true
+}
